b0d1477fde1a2d5ff98da3f5dacc916c5381c76d
jennileal <jhenneffer345@gmail.com>
2024-07-01 18:48:20 -0300

    Aula com if simples 1.0.0
    Aprendemos como comentar em linha simples, em bloco, oque é variavel, como ela atribui dados um print que exibe no formato string ou texto

---
+++ python_if.py
@@ -0,0 +1,22 @@
+"""
+Este programa é uma estrutura IF simples
+"""
+valor = 13 # A variavel valor recebeu o número 10
+"""
+valor é a variavel
+= atribuiu um dado à variavel valor
+10 é um daddo númérico    
+ """
+if valor > 5:
+    
+    """
+    if é o msm que SE CONDIÇÃO
+    valor é a variavel que recebeu um dado numerico
+    >5 se o dado for maior que 5
+    """
+    print('O valor é maior que 5.')
+    
+    """
+    A informação exibida no print pode ser entre aspas ou apostrofos
+    pq é um dado tipo texto, ou string.
+    """
